0fa88b938a28d03f0864ee4a562f6991e7b772f6
LazyukAleksandr <Zuk8080@mail.ru>
2022-07-04 21:15:50 +0300

    Added rus comment

--- Domino.py
+++ Domino.py
@@ -1,104 +1,135 @@
 from random import shuffle
 from itertools import combinations_with_replacement, chain
 from collections import Counter
-# define turn function
+
+
+# определяем функцию поворота кости
 def turn_func(func_input, func_pieces):
-    # stop if there is no pieces
+    # стоп если нет кости
     if int(func_input) == 0 and len(stock_pieces) == 0:
         return None
-    # give piece to player
+
+
+    # дать ход игроку
     elif int(func_input) == 0 and len(stock_pieces) > 0:
         func_pieces.append(stock_pieces[-1])
         stock_pieces.remove(stock_pieces[-1])
         return None
-    # place piece right after snake
+
+
+    # расположить кость справа от змейки
     if int(func_input) > 0:
-        # get piece from player or computer
+        # получить кость от игрока или компьютера
         piece_to_end = func_pieces[int(func_input) - 1]
-        # reverse piece
+        # повернуть кость
         if piece_to_end[1] == snake[-1][-1]:
             piece_to_end.reverse()
-        # place piece
+        # положить на место
         snake.append(piece_to_end)
-        # remove piece from player or computer
+        # удалить кость
         func_pieces.remove(func_pieces[int(func_input) - 1])
-    # place piece left after snake
+
+    # расположить кость слева от змейки
     else:
-        # get piece from player or computer
+        # получить кость от игрока или компьютера
         piece_to_start = func_pieces[-int(func_input) - 1]
-        # reverse piece
+        # реверс кости
         if piece_to_start[0] == snake[0][0]:
             piece_to_start.reverse()
-        # place piece
+        # положить кость
         snake.insert(0, piece_to_start)
-        # remove piece from player or computer
+        # удалить кость
         func_pieces.remove(func_pieces[-int(func_input) - 1])
-# check if this snake is winning
+
+
+# проверяем, выигрывает ли эта змейка
 def win_snake(func_snake):
     if func_snake[0][0] == func_snake[-1][-1] and sum(x.count(func_snake[0][0]) for x in func_snake) == 8:
         return True
-# define list of dominoes
+
+
+# определяем список костяшек
 dominoes = list(combinations_with_replacement(range(0, 7), 2))
-# convert list of tuples to list of lists
+
+# преобразовать список кортежей в список списков
 dominoes = [list(x) for x in dominoes]
-# shuffle dominoes
+
+# тосуем колоду
 shuffle(dominoes)
-# define coefficient equal to half of the number of dominoes
+
+# определяем коэффициент равный половине числа костяшек домино
 coefficient = len(dominoes) // 2
-# get first half of the dominoes
+
+# получить первую половину домино
 stock_pieces = dominoes[:coefficient]
-# get computer's and player's pieces
+
+# получить части компьютера и игрока
 computer_pieces = dominoes[coefficient:int(coefficient * 1.5)]
 player_pieces = dominoes[int(coefficient * 1.5):]
-# find snake
+
+# найти змейку
 snake = [max([[x, y] for x, y in computer_pieces + player_pieces if x == y])]
-# remove snake from computer's or player's pieces
+
+# удалить змею из фигур компьютера или игрока
 computer_pieces.remove(snake[0]) if snake[0] in computer_pieces else player_pieces.remove(snake[0])
-# define who is first
+
+# кто ходит первый
 turn_num = 0 if len(player_pieces) > len(computer_pieces) else 1
-# start game
+
+# старт игры
 while True:
-    # show stock, player's and computer's pieces
+
+    # показать стоковые, игровые и компьютерные фишки
     print('=' * 70)
     print('Stock size:', len(stock_pieces))
     print('Computer pieces:', len(computer_pieces), '\n')
     print(*snake, '\n', sep='') if len(snake) <= 6 else print(*snake[:3], '...', *snake[-3:], '\n', sep='')
     print("Your pieces:")
+
     for num, piece in enumerate(player_pieces):
         print(f"{num + 1}: {piece}")
-    # condition for player's win
+
+    # условие победы игрока
     if len(player_pieces) == 0 or win_snake(snake) and turn_num == 0:
         print("\nStatus: The game is over. You won!")
         break
-    # condition for computer's win
+
+
+    # условие победы компьютера
     if len(computer_pieces) == 0 or win_snake(snake) and turn_num == 1:
         print("\nStatus: The game is over. The computer won!")
         break
-    # define snake ends
+
+
+    # определить концы змеи
     connection_keys = [snake[0][0], snake[-1][-1]]
-    # condition for draw
+    # условие розыгрыша
     if len(stock_pieces) == 0 and \
             not any(item in connection_keys for item in list(chain(*(player_pieces + computer_pieces)))):
         print("\nStatus: The game is over. It's a draw!")
         break
-    # player's turn
+
+
+    # ход игрока
     if turn_num % 2 == 0:
-        # count turn number
+        # считаем номер хода
         turn_num += 1
-        # show message
+        # сообщение
         print("\nStatus: It's your turn to make a move. Enter your command.")
-        # get player's input
+        # ввод игрока
         user_input = input()
-        # check if player's input is valid
+        # проверка валидности комманды игрока
         if user_input.lstrip("-").isdigit() and int(user_input) in range(-len(player_pieces), len(player_pieces) + 1):
-            # provide piece to player
+            # предоставить кость игроку
             if int(user_input) == 0:
                 turn_func(user_input, player_pieces)
                 continue
-            # define current piece
+
+            # определить текущую часть
             current_piece = player_pieces[int(user_input) - 1] if int(user_input) > 0 \
                 else player_pieces[-int(user_input) - 1]
-            # check if piece is valid
+
+            # проверяем часть кости на валидность
             if connection_keys[-1] in current_piece and int(user_input) > 0 or \
                     connection_keys[0] in current_piece and int(user_input) < 0:
                 turn_func(user_input, player_pieces)
@@ -110,33 +141,33 @@ while True:
             print("Invalid input. Please try again.")
             turn_num -= 1
             continue
-    # computer's turn
+    # ход компьютера
     else:
-        # count turn number
+        # считаем номер хода компьютера
         turn_num += 1
-        # show message
+        # сообщение
         print("\nStatus: Computer is about to make a move. Press Enter to continue...")
-        # wait for player's input
+        # комманда компьютера
         input()
-        # count number on pieces in snake and computer's pieces
+        # считать количество частей в змейке и в частях компьютера
         count_nums = Counter(chain(*(computer_pieces + snake)))
-        # define scores for each piece
+        # определить баллы
         scores = list()
-        # iterate through all pieces to get scores
+        # перебрать все части, чтобы получить очки
         for piece in computer_pieces:
             score = count_nums[piece[0]] + count_nums[piece[1]]
             scores.append(score)
-        # sort pieces by scores
+        # сортировать произведения по баллам
         computer_pieces = [x for _, x in sorted(zip(scores, computer_pieces), reverse=True)]
-        # make computer's move
+        # заставить компьютер двигаться
         for piece in computer_pieces:
-            # check how to connect snake
+            # проверить, как подключить змейку
             if connection_keys[-1] in piece:
                 turn_func(str(computer_pieces.index(piece) + 1), computer_pieces)
                 break
             elif connection_keys[0] in piece:
                 turn_func(str(-computer_pieces.index(piece) - 1), computer_pieces)
                 break
-        # provide piece to computer
+        # предоставить кость компьютеру
         else:
             turn_func('0', computer_pieces)
